d08437601e7e17716d02033abd68743d938dfbff
svramanaiah <svramana1998@gmail.com>
2025-08-20 19:58:14 +0530

    first commit

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-c8aetlj(=vp90n@#yoc^&d(_6ivp(d!bv-4-f!r$lawptjzrwu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
